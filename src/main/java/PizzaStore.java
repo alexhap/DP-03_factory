0fb29084dc077a7a26572bc038cd6632e0674f37
alexhap <hap@ukr.net>
2015-06-18 21:05:23 +0300

    abstract factory implemented

--- src/main/java/PizzaStore.java
+++ src/main/java/PizzaStore.java
@@ -2,9 +2,11 @@
  * Created by alex on 17.06.2015.
  *
  */
+
 import java.util.*;
 
 public abstract class PizzaStore {
+
     public Pizza orderPizza(String type) {
         Pizza pizza;
         pizza = createPizza(type);
@@ -14,9 +16,11 @@ public abstract class PizzaStore {
         pizza.box();
         return pizza;
     }
+
     protected abstract Pizza createPizza(String type);
 
     public static void main(String args[]) {
+
         List<PizzaStore> lst = new ArrayList<PizzaStore>();
         lst.add(new NYPizzaStore());
         lst.add(new CaliforniaPizzaStore());
@@ -26,5 +30,6 @@ public abstract class PizzaStore {
         for (PizzaStore ps : lst) ps.orderPizza("cheese");
         for (PizzaStore ps : lst) ps.orderPizza("clam");
         for (PizzaStore ps : lst) ps.orderPizza("veggie");
+
     }
 }
