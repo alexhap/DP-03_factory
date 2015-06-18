dd42bf520bf6f179948d65028e6aea8fe1cd0b5f
alexhap <hap@ukr.net>
2015-06-18 15:46:39 +0300

    patched

--- src/main/java/PizzaStore.java
+++ src/main/java/PizzaStore.java
@@ -13,6 +13,7 @@ public abstract class PizzaStore {
         pizza.cut();
         pizza.box();
         return pizza;
+//        return createPizza(type).prepare().bake().cut().box();
     }
     protected abstract Pizza createPizza(String type);
 
